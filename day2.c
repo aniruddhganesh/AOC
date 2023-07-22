6574b445a5bbaa4514b8b2ed35973b47bcbfd194
Lark <lark@Houdini>
2023-07-22 11:30:32 +0530

    Completed Day2 : Part 2

--- day2.c
+++ day2.c
@@ -2,12 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+//#define PART_1 
 /* A     B       C  [Opponent]
  * Rock  Paper   Scisors
  * X     Y       Z  [US]
  * +1   +2      +3
  * 0 : lost, +3 : draw, +6 : Win
  */
+
 int calc_shape(int mov_self)
 {
     int pts_shape = 0;
@@ -25,6 +27,30 @@ int calc_shape(int mov_self)
     return pts_shape;
 }
 
+// Takes Opponent move, and required outcome,
+// Returns the Move we should play "mov_self"
+int outcome_shape(int mov_opp, int result_shape)
+{
+    int mov_self;
+
+    if (result_shape == 'X') {
+        if (mov_opp == 'B' || mov_opp == 'C')
+            mov_self = (mov_opp -1);
+        else
+            mov_self = (mov_opp + 2);
+    }
+    else if (result_shape == 'Z'){
+        if (mov_opp == 'A' || mov_opp == 'B') // If needed to win
+            mov_self = (mov_opp + 1);   // Add 1 if Rock or Paper
+        else
+            mov_self = (mov_opp - 2); // Remove 2 If Scissors
+    }
+    else {
+        mov_self = mov_opp;
+    }
+    
+    return mov_self;
+}
 int calc_result(int mov_self, int mov_opp)
 {
     int pts_result = 0;
@@ -40,11 +66,6 @@ int calc_result(int mov_self, int mov_opp)
     return pts_result;
 }
 
-int part_two(void)
-{
-    return 0;
-}
-
 int main(void)
 {
     FILE *fp = fopen("./day2.txt", "r");
@@ -55,10 +76,21 @@ int main(void)
     // In part 1 Opponent is left column, self right
     while(fgets(buffer, 8, fp) != NULL) {
         mov_opp = buffer[0];
+#ifdef PART_1
         mov_self = (buffer[2] - 23); // Equates ie; X becomes A; Y becomes B...
+#else
+        char result_shape = buffer[2];
+        mov_self = outcome_shape(mov_opp, result_shape);
+#endif
         pts_total += (calc_result(mov_self, mov_opp) + calc_shape(mov_self));
     }
 
+#ifdef PART_1 
     printf("%d Total --> Part 1 Solution!\n", pts_total);
     return 0;
+#else
+    printf("%d Total --> Part 2 Solution!\n", pts_total);
+    return 0;
+#endif
+
 }
