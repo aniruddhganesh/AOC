3660d4a51b9ff6a36cb83fd7111a24de1d74f904
Lark <lark@Houdini>
2023-07-22 09:52:27 +0530

    Merge remote changes into main

--- day2.c
+++ day2.c
@@ -8,8 +8,10 @@
  * +1   +2      +3
  * 0 : lost, +3 : draw, +6 : Win
  */
-int pts_count(int mov_self, int mov_opp) {
-    int pts_result = 0, pts_shape = 0;
+int calc_shape(int mov_self)
+{
+    int pts_shape = 0;
+    // Calculate shape bonus
     switch (mov_self) { // WORKS!
         case 'A': 
             pts_shape += 1; break; // Rock +1
@@ -20,7 +22,12 @@ int pts_count(int mov_self, int mov_opp) {
         default:
             exit(EXIT_FAILURE); // IDK man error handling
     }
+    return pts_shape;
+}
 
+int calc_result(int mov_self, int mov_opp)
+{
+    int pts_result = 0;
     // Calculate Victory or Loss
     if (mov_opp != mov_self) { // Break through if draw
         if ((mov_opp - mov_self) == 2 || (mov_opp - mov_self) == -1)
@@ -30,9 +37,14 @@ int pts_count(int mov_self, int mov_opp) {
     else {
         pts_result += 3; // If moves are equal draw!
     }
-
-    return (pts_result + pts_shape);
+    return pts_result;
 }
+
+int part_two(void)
+{
+    return 0;
+}
+
 int main(void)
 {
     FILE *fp = fopen("./day2.txt", "r");
@@ -40,13 +52,13 @@ int main(void)
     char mov_self, mov_opp;
     int pts_total = 0;
 
-
+    // In part 1 Opponent is left column, self right
     while(fgets(buffer, 8, fp) != NULL) {
         mov_opp = buffer[0];
         mov_self = (buffer[2] - 23); // Equates ie; X becomes A; Y becomes B...
-        pts_total += pts_count(mov_self, mov_opp);
+        pts_total += (calc_result(mov_self, mov_opp) + calc_shape(mov_self));
     }
 
-    printf("%d Total\n", pts_total);
+    printf("%d Total --> Part 1 Solution!\n", pts_total);
     return 0;
 }
